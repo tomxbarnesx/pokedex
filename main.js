eab4504905b275680f90c18b6fc35ae5e6368b93
Tom Barnes <barnes.thomas.c@gmail.com>
2018-07-18 12:57:06 -0400

    ditto button started

--- main.js
+++ main.js
@@ -39,8 +39,29 @@ let render = (targetPoke) => {
 
     let abilityDisplay1 = document.getElementsByClassName("ability-display")[0];
     let abilityDisplay2 = document.getElementsByClassName("ability-display")[1];
-    abilityDisplay1.innerText = PokeTom.party[targetPoke].abilities[0];
-    abilityDisplay2.innerText = PokeTom.party[targetPoke].abilities[1];
+
+    let ability1 = PokeTom.party[targetPoke].abilities[0];
+    if(ability1.includes("-")){
+        let abilityArray = ability1.split("");
+        console.log(abilityArray.indexOf("-") + 1);
+        abilityArray.splice(ability1.indexOf("-"), 1, " ");
+        ability1 = abilityArray.join("");
+        abilityLetter = ability1.charAt(ability1.indexOf(" ") + 1).toUpperCase();
+        ability1 = ability1.charAt(0).toUpperCase() + ability1.slice(1, ability1.indexOf(" ") + 1) + ability1.charAt(ability1.indexOf(" ") + 1).toUpperCase() + ability1.slice(ability1.indexOf(" ") + 2);
+    };
+
+    let ability2 = PokeTom.party[targetPoke].abilities[1];
+    if(ability2.includes("-")){
+        let abilityArray = ability2.split("");
+        console.log(abilityArray.indexOf("-") + 1);
+        abilityArray.splice(ability2.indexOf("-"), 1, " ");
+        ability2 = abilityArray.join("");
+        abilityLetter = ability2.charAt(ability2.indexOf(" ") + 1).toUpperCase();
+        ability2 = ability2.charAt(0).toUpperCase() + ability2.slice(1, ability2.indexOf(" ") + 1) + ability2.charAt(ability2.indexOf(" ") + 1).toUpperCase() + ability2.slice(ability2.indexOf(" ") + 2);
+    };
+
+    abilityDisplay1.innerText = ability1;
+    abilityDisplay2.innerText = ability2;
 
     let HPBar = document.getElementsByClassName("bar-fill")[0];
     let HPAnimation = document.getElementsByClassName("animating-bar")[0];
@@ -128,6 +149,22 @@ searchButton.addEventListener("click", (event) => {
 
     })
 });
+
+//DITTO CLICK
+
+let dittoButton = document.getElementById("ditto");
+
+dittoButton.addEventListener("click", (event) => {
+    let randomizer = (min, max) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    let randomPokeNum = randomizer(1, 807);
+
+    axios.get("https://pokeapi.co/api/v2/pokemon/" + searchInput.toLowerCase() + "/").then((response) => {
+})
 
 //TOGGLES + SOUNDS
 
