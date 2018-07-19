7562f9cb4227c9da9f605fcb0b9d5ac48e4a9d3d
Tom Barnes <barnes.thomas.c@gmail.com>
2018-07-19 11:10:51 -0400

    loading complete

--- main.js
+++ main.js
@@ -31,17 +31,26 @@ $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   })
   
+//RENDER DEFINITIONS
+let pokeNameDisplay = document.getElementById("poke-name");
+let avatarDisplay = document.getElementById("avatar");
+let abilityDisplay1 = document.getElementsByClassName("ability-display")[0];
+let abilityDisplay2 = document.getElementsByClassName("ability-display")[1];
+let HPBar = document.getElementsByClassName("bar-fill")[0];
+let HPAnimation = document.getElementsByClassName("animating-bar")[0];
+let ATKBar = document.getElementsByClassName("bar-fill")[1];
+let ATKAnimation = document.getElementsByClassName("animating-bar")[1];
+let DEFBar = document.getElementsByClassName("bar-fill")[2];
+let DEFAnimation = document.getElementsByClassName("animating-bar")[2];
+
 //RENDER FUNCTION
 
 let render = (targetPoke) => {
-    let pokeNameDisplay = document.getElementById("poke-name");
     pokeNameDisplay.innerText = PokeTom.party[targetPoke].name;
 
-    let avatarDisplay = document.getElementById("avatar");
     avatarDisplay.src = PokeTom.party[targetPoke].avatar;
-
-    let abilityDisplay1 = document.getElementsByClassName("ability-display")[0];
-    let abilityDisplay2 = document.getElementsByClassName("ability-display")[1];
+    avatarDisplay.height = "350";
+    avatarDisplay.width = "350";
 
     let ability1 = PokeTom.party[targetPoke].abilities[0];
     if(ability1.includes("-")){
@@ -66,16 +75,10 @@ let render = (targetPoke) => {
     abilityDisplay1.innerText = ability1;
     abilityDisplay2.innerText = ability2;
 
-    let HPBar = document.getElementsByClassName("bar-fill")[0];
-    let HPAnimation = document.getElementsByClassName("animating-bar")[0];
     HPBar.style.width = String(((319 - PokeTom.party[targetPoke].hp) / 319) * 100) + "%";
 
-    let ATKBar = document.getElementsByClassName("bar-fill")[1];
-    let ATKAnimation = document.getElementsByClassName("animating-bar")[1];
     ATKBar.style.width = String(((238 - PokeTom.party[targetPoke].attack) / 238) * 100) + "%";
 
-    let DEFBar = document.getElementsByClassName("bar-fill")[2];
-    let DEFAnimation = document.getElementsByClassName("animating-bar")[2];
     DEFBar.style.width = String(((288 - PokeTom.party[targetPoke].defense) / 288) * 100) + "%";
 
     HPAnimation.classList.add("filling");
@@ -115,6 +118,15 @@ axios.get("https://pokeapi.co/api/v2/pokemon/vileplume/").then((response) => {
 
 searchButton.addEventListener("click", (event) => {
     searchBeep.play();
+    
+    //LOADING:
+    pokeNameDisplay.innerText = "Loading...";
+    avatarDisplay.src = "Dancing_Ditto.gif";
+    avatarDisplay.height="200";
+    avatarDisplay.width="200";
+    abilityDisplay1.innerText = "Loading...";
+    abilityDisplay2.innerText = "Loading...";
+
     let searchInput = document.getElementsByClassName("search-input")[0].value;
     
     // axios.get("https://pokeapi-nycda.firebaseio.com/pokemon/" + searchInput + ".json")
@@ -199,6 +211,14 @@ dittoButton.addEventListener("click", (event) => {
 
     let randomPokeNum = randomizer(1, 807);
 
+    //LOADING:
+    pokeNameDisplay.innerText = "Loading...";
+    avatarDisplay.src = "Dancing_Ditto.gif";
+    avatarDisplay.height="200";
+    avatarDisplay.width="200";
+    abilityDisplay1.innerText = "Loading...";
+    abilityDisplay2.innerText = "Loading...";
+
     axios.get("https://pokeapi.co/api/v2/pokemon/" + randomPokeNum + "/").then((response) => {
         data = response.data;
 
@@ -236,8 +256,8 @@ dittoButton.addEventListener("click", (event) => {
 
 //TOGGLES
 
-let toggleLeft = document.getElementsByClassName("toggle-arrow")[0];
-let toggleRight = document.getElementsByClassName("toggle-arrow")[1];
+let toggleLeft = document.getElementsByClassName("toggle-button")[0];
+let toggleRight = document.getElementsByClassName("toggle-button")[2];
 let beep = document.getElementById("toggle-beep");
 let searchBeep = document.getElementById("search-beep");
 
@@ -263,3 +283,59 @@ toggleRight.addEventListener("click", (event) => {
         render(current);
         console.log(current);
 });
+
+
+// let render = (targetPoke) => {
+//     let pokeNameDisplay = document.getElementById("poke-name");
+//     pokeNameDisplay.innerText = PokeTom.party[targetPoke].name;
+
+//     let avatarDisplay = document.getElementById("avatar");
+//     avatarDisplay.src = PokeTom.party[targetPoke].avatar;
+
+//     let abilityDisplay1 = document.getElementsByClassName("ability-display")[0];
+//     let abilityDisplay2 = document.getElementsByClassName("ability-display")[1];
+
+//     let ability1 = PokeTom.party[targetPoke].abilities[0];
+//     if(ability1.includes("-")){
+//         let abilityArray = ability1.split("");
+//         console.log(abilityArray.indexOf("-") + 1);
+//         abilityArray.splice(ability1.indexOf("-"), 1, " ");
+//         ability1 = abilityArray.join("");
+//         abilityLetter = ability1.charAt(ability1.indexOf(" ") + 1).toUpperCase();
+//         ability1 = ability1.charAt(0).toUpperCase() + ability1.slice(1, ability1.indexOf(" ") + 1) + ability1.charAt(ability1.indexOf(" ") + 1).toUpperCase() + ability1.slice(ability1.indexOf(" ") + 2);
+//     };
+
+//     let ability2 = PokeTom.party[targetPoke].abilities[1];
+//     if(ability2.includes("-")){
+//         let abilityArray = ability2.split("");
+//         console.log(abilityArray.indexOf("-") + 1);
+//         abilityArray.splice(ability2.indexOf("-"), 1, " ");
+//         ability2 = abilityArray.join("");
+//         abilityLetter = ability2.charAt(ability2.indexOf(" ") + 1).toUpperCase();
+//         ability2 = ability2.charAt(0).toUpperCase() + ability2.slice(1, ability2.indexOf(" ") + 1) + ability2.charAt(ability2.indexOf(" ") + 1).toUpperCase() + ability2.slice(ability2.indexOf(" ") + 2);
+//     };
+
+//     abilityDisplay1.innerText = ability1;
+//     abilityDisplay2.innerText = ability2;
+
+//     let HPBar = document.getElementsByClassName("bar-fill")[0];
+//     let HPAnimation = document.getElementsByClassName("animating-bar")[0];
+//     HPBar.style.width = String(((319 - PokeTom.party[targetPoke].hp) / 319) * 100) + "%";
+
+//     let ATKBar = document.getElementsByClassName("bar-fill")[1];
+//     let ATKAnimation = document.getElementsByClassName("animating-bar")[1];
+//     ATKBar.style.width = String(((238 - PokeTom.party[targetPoke].attack) / 238) * 100) + "%";
+
+//     let DEFBar = document.getElementsByClassName("bar-fill")[2];
+//     let DEFAnimation = document.getElementsByClassName("animating-bar")[2];
+//     DEFBar.style.width = String(((288 - PokeTom.party[targetPoke].defense) / 288) * 100) + "%";
+
+//     HPAnimation.classList.add("filling");
+//     ATKAnimation.classList.add("filling");
+//     DEFAnimation.classList.add("filling");
+
+//     setTimeout(() => {
+//         HPAnimation.classList.remove("filling");
+//         ATKAnimation.classList.remove("filling");
+//         DEFAnimation.classList.remove("filling");}, 1600);
+// }
